6e45cf5805ffa0ab67df3c1a812996a4baed42df
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-29 09:22:35 +0530

    Update CalculateDistance.java

--- CalculateDistance.java
+++ CalculateDistance.java
@@ -1,3 +1,10 @@
+/*A Cartesian plane is defined by two perpendicular number lines: the x-axis, which is horizontal, and the y-axis, which is vertical.
+Using these axes, we can describe any point in the plane using an ordered pair of numbers.
+
+Task:
+Write a program which takes two different points, A(x1,y1) and B(x2,y2), and returns the distance between these two points.
+The distance formula is the Pythagorean Theorem:
+*/
 import java.util.Scanner;
 import java.lang.Math;
 public class CalculateDistance{
