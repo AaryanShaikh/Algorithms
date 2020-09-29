4ead96562befabe43bb3e4fe094180fcf927f44a
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-29 09:16:04 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,3 +1,6 @@
+//An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+//For instance, a 3 digit number will be considered an Armstrong number if the sum of the cubes of its digits is equal to the number itself. 
+//For example, 153 is an Armstrong number, as 1*3 + 53 + 3*3 = 153
 import java.util.*;
 public class Armstrong
 {
