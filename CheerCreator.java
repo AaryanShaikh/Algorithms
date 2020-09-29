84846fa2eac03d270684c8b299bdc97ebcbe3794
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-29 09:24:31 +0530

    Update CheerCreator.java

--- CheerCreator.java
+++ CheerCreator.java
@@ -1,3 +1,11 @@
+/*You are cheering on your favorite team. After each play, if your team got over 10 yards further down the field, you stand up and give your friend a high five. 
+If they don't move forward by at least a yard you stay quiet and say 'shh', and if they move forward 10 yards or less, you say 'Ra!' for every yard that they moved
+forward in that play.
+Sample Input 
+3
+Sample Output
+Ra!Ra!Ra!
+*/
 import java.util.*;
 public class CheerCreator{
 	public static void main(String[] args) {
