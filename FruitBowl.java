6a8b20d7db8b0b96d540a56625c01944310e5586
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-29 09:49:10 +0530

    Update FruitBowl.java

--- FruitBowl.java
+++ FruitBowl.java
@@ -1,3 +1,9 @@
+/*You have a bowl on your counter with an even number of pieces of fruit in it. Half of them are bananas, and the other half are apples. You need 3 apples to make a pie.
+Sample Input
+26 
+Sample Output 
+4
+*/
 import java.util.Scanner;
 public class FruitBowl{
 	public static void main(String[] args) {
