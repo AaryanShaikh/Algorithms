5137fca66cf78b433c876d656d68a4466d6f51b2
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-29 09:55:46 +0530

    Update GothamCity.java

--- GothamCity.java
+++ GothamCity.java
@@ -1,3 +1,10 @@
+/*You are a police officer, and you get a report of criminal activity! Should you go on your own, or should you call a superhero to help you fight the crime? If there 
+are less than 5, you can handle this on your own, if there are 5-10, you will want to go with Batman for backup, and if there are more than 10, you should stay where it is
+safe and let Batman handle this on his own!
+Sample Input: 
+7
+Sample Output: 
+'Help me Batman'*/
 import java.util.Scanner;
 public class GothamCity{
 	public static void main(String[] args) {
