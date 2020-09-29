71e48bc4d09264ff400a52b458daaf62e5f6caa1
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-29 09:20:40 +0530

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,6 +1,9 @@
 /*The Caesar cipher is a type of substitution cipher in which each letter in the plaintext is shifted to a certain number of places down the alphabet. 
 For example, with a shift of 1, A would be replaced by B, B would become C, and so on. The method is named after Julius Caesar, who allegedly used it to communicate 
-with his generals.*/
+with his generals.
+For example:
+Input: "defend the east wall of the castle"
+Output: "efgfoe uif fbtu xbmm pg uif dbtumf"*/
 import java.util.*;
 public class CaesarCipher
 {
