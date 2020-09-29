05ace39c0c6c7f0e3b75a7357934b21e79e34829
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-29 09:58:14 +0530

    Update Hovercraft.java

--- Hovercraft.java
+++ Hovercraft.java
@@ -1,3 +1,13 @@
+/*You run a hovercraft factory. Your factory makes ten hovercrafts in a month. Given the number of customers you got that month, did you make a profit? It costs you
+2,000,000 to build a hovercraft, and you are selling them for 3,000,000. You also pay 1,000,000 each month for insurance.
+Input Format: 
+An integer that represents the sales that you made that month.
+Output Format: 
+A string that says 'Profit', 'Loss', or 'Broke Even'.
+Sample Input: 
+5
+Sample Output: 
+Loss*/
 import java.util.Scanner;
 public class Hovercraft{
 	public static void main(String[] args) {
