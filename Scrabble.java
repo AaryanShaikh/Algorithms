d27a04d994520c047379f3c1bb804a21245ac366
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 15:56:37 +0530

    Update Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -1,3 +1,15 @@
+/* Give a variable number of words, create a program to output the word with the maximum score, based on the following scoring system:
+The score of a word is calculated based on its character values, according to this table:
+e, a, i, o, n, r, t, l, s, u are equal 1 point
+d, g	2 points
+b, c, m, p	3 points
+f, h, v, w, y 4 points
+k 5 points
+j, x 8 points
+q, z 10 points
+input { "This",  "is", "a", "cool" , "Challenge" }
+output : Challenge
+*/
 import java.util.*;
 public class Scrabble{
 	public static void main(String[] args) {
