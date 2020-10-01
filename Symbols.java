b3838aa829c0d9838717abf5d73b18a91562d8bf
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 16:11:00 +0530

    Update Symbols.java

--- Symbols.java
+++ Symbols.java
@@ -1,3 +1,10 @@
+/* There is a problem with your keyboard: it randomly writes symbols when you are typing a text. You need to clean up the text by removing all symbols.
+Take a text that includes some random symbols and translate it into a text that has none of them. The resulting text should only include letters and numbers.
+Sample Input: 
+#l$e%ts go @an#d@@ g***et #l#unch$$$
+Sample Output: 
+lets go and get lunch
+*/
 import java.util.*;
 public class Symbols{
 	public static void main(String[] args) {
