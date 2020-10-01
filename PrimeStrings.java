42ebd5a46c5ada754c4f839f07f217f76870f255
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 15:44:22 +0530

    Update PrimeStrings.java

--- PrimeStrings.java
+++ PrimeStrings.java
@@ -1,3 +1,13 @@
+/*A String is called prime if it can't be constructed by concatenating multiple (more than one) equal strings.
+"abac" is prime, but "xyxy" is not ("xyxy" = "xy" + "xy").
+Implement a program which outputs whether a given string is prime.
+Input: "xyxy"
+Output: "not prime"
+Input: "aaaa"
+Output: "not prime"
+Input: "hello"
+Output: "prime"
+*/
 import java.util.*;
 public class PrimeStrings{
 	public static void main(String[] args) {
