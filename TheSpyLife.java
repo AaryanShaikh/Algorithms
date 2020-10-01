b37b3a327d729b8f132f065eae83784c8bc45204
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 16:15:41 +0530

    Update TheSpyLife.java

--- TheSpyLife.java
+++ TheSpyLife.java
@@ -1,3 +1,6 @@
+/* A number is called a Spy number, if the sum and product of its digits are equal.
+For example, 123 is a Spy number, as the sum of its digits is 6 (1+2+3) which is equal to the product of its digits: 6 (1x2x3).
+*/
 import java.util.*;
 public class TheSpyLife{
 	public static void main(String[] args) {
