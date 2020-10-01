bc75b717d2a1e26b546f65d9c6d104829de9ac37
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 15:34:02 +0530

    Update Paranoia.java

--- Paranoia.java
+++ Paranoia.java
@@ -1,3 +1,23 @@
+/*ByteCorp is a famous technological company in Byteania. 
+The CEO of ByteCorp doesn't trust anyone and thinks that his accountant managed to move huge amounts of money to a competitor company, MegaCorp.
+He hires a computer crime investigator, and asks him to find inconsistencies in the money transfers.
+Here is a sample transaction log of the company:
+Feb SLR 4 M
+Feb ENT 800 K
+Mar SLR 4000 K
+Mar ENT 800 K
+Apr SLR 4010 K
+Apr ENT 810 K
+
+There are four columns:
+1. Month of the transaction
+2. Reason of the expense (SLR for "salary", ENT for "entertainment", OTR for "other")
+3. Amount
+4. M, K, or B (M for million, K for thousands, B for billion)
+
+In the example above, April expenses show an inconsistency and should be reported.
+As the computer investigator, write a program, which reads the transaction logs, detects inconsistent expenses and prints the exact month containing the "unusual" activities.
+*/
 import java.util.*;
 public class Paranoia{
 	public static void main(String[] args) {
