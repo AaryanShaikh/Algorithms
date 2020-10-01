f8aa6b61552883d852e364ef176a87769839f5e4
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 15:40:12 +0530

    Update Popsicles.java

--- Popsicles.java
+++ Popsicles.java
@@ -1,3 +1,10 @@
+/* You have a box of popsicles and you want to give them all away to a group of brothers and sisters. If you have enough left in the box to give them each an even amount
+you should go for it! If not, they will fight over them, and you should eat them yourself later.
+Sample Input
+3 9
+Sample Output
+give away
+*/
 import java.util.*;
 public class Popsicles{
 	public static void main(String[] args) {
