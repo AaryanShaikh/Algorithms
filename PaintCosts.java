c83be3b3b306483ff92fd74b6c92ec6b02b3aba4
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 15:29:23 +0530

    Update PaintCosts.java

--- PaintCosts.java
+++ PaintCosts.java
@@ -1,3 +1,12 @@
+/*You are getting ready to paint a piece of art. The canvas and brushes that you want to use will cost 40.00. Each color of paint that you buy is an additional 5.00.
+Determine how much money you will need based on the number of colors that you want to buy if tax at this store is 10%.
+Task 
+Given the total number of colors of paint that you need, calculate and output the total cost of your project rounded up to the nearest whole number.
+Sample Input 
+10
+Sample Output 
+99
+*/
 import java.util.*;
 public class PaintCosts{
 	public static void main(String[] args) {
