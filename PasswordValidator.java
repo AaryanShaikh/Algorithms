461cf13fea1069a4d9df3a4816712e90a4519671
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 15:37:05 +0530

    Update PasswordValidator.java

--- PasswordValidator.java
+++ PasswordValidator.java
@@ -1,3 +1,12 @@
+/* Password validator is a program that validates passwords to match specific rules. For example, the minimum length of the password must be eight characters long and it
+should have at least one uppercase letter in it.
+A valid password is the one that conforms to the following rules:
+ - Minimum length is 5;
+ - Maximum length is 10;
+ - Should contain at least one number;
+ - Should contain at least one special character (such as &, +, @, $, #, %, etc.);
+ - Should not contain spaces.
+*/
 import java.util.*;
 public class PasswordValidator{
 	public static void main(String[] args){
