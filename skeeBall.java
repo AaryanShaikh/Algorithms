39d2f04a0600bc6b20a8257cde70dab86addf6b1
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 16:22:37 +0530

    Update skeeBall.java

--- skeeBall.java
+++ skeeBall.java
@@ -1,3 +1,13 @@
+/* You are playing a game at your local arcade, and you receive 1 ticket from the machine for every 12 points that you score. You want to purchase a squirt gun with your
+tickets. Given your score, and the price of the squirt gun (in tickets) are you able to buy it?
+Task 
+Evaluate whether or not you have scored high enough to earn enough tickets to purchase the squirt gun at the arcade.
+Sample Input 
+500
+40
+Sample Output
+Buy it!
+*/
 import java.util.*;
 public class skeeBall{
 	public static void main(String[] args) {
