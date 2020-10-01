67c31680cbc912ffed6fa793b8481412a909637a
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-01 15:26:20 +0530

    Update NoNumerals.java

--- NoNumerals.java
+++ NoNumerals.java
@@ -1,3 +1,10 @@
+/*You write a phrase and include a lot of number characters (0-9), but you decide that for numbers 10 and under you would rather write the word out instead. Can you go 
+in and edit your phrase to write out the name of each number instead of using the numeral?
+Sample Input: 
+i need 2 pumpkins and 3 apples
+Sample Output: 
+i need two pumpkins and three apples
+*/
 import java.util.Scanner;
 public class NoNumerals{
 	public static void main(String[] args) {
