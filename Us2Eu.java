a16c9e8308f0fb907ecb020a54acb2639527fdb8
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-30 00:09:10 +0530

    Update Us2Eu.java

--- Us2Eu.java
+++ Us2Eu.java
@@ -1,3 +1,16 @@
+/*You and your friends are going to Europe! You have made plans to travel around Europe with your friends, but one thing you need to take into account so that everything goes
+according to play, is that the format of their date is different than from what is used in the United States. Your job is to convert all your dates from MM/DD/YYYY to DD/MM/YYYY.
+Task: 
+Create a function that takes in a string containing a date that is in US format, and return a string of the same date converted to EU.
+Input Format: 
+A string that contains a date formatting 11/19/2019 or November 19, 2019.
+Output Format: 
+A string of the same date but in a different format: 19/11/2019.
+Sample Input: 
+7/26/2019
+Sample Output: 
+26/7/2019
+*/
 import java.util.*;
 public class Us2Eu{
 	public static void main(String[] args) {
