af4588130fe8ff33f76f8e835631af117a9cc2c7
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-30 00:33:29 +0530

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -1,5 +1,9 @@
-/*A customer purchases one or more items and gives the respective quantity for each item to be purchased. Based on the quantity and price of the items, display the final amount after discount. A customer is eligible for 10% miscount on total purchase price above R.,11000. For total purchase price below Rs.1000, no discount is given. Check if customer has a membership with the store. If he is member, then he gets an extra 5% discount on every purchase, but if purchase amount of member is above Rs.1000 then he gets total discount of 15% on purchase price. For example, if RaJesh does purchase of Rs.1800 which is above Rs.1000 and he is not member of store he gets discount of 10%.
-But If Rajesh Is member of store then he gets total 15% discount on Rs.1800 since its above Rs.1000, but if he does purchase of Rs.800 which Is below Rs.1000 then he gets only discount of 5%.
+/*A customer purchases one or more items and gives the respective quantity for each item to be purchased. Based on the quantity and price of the items, display the final 
+amount after discount. A customer is eligible for 10% miscount on total purchase price above R.,11000. For total purchase price below Rs.1000, no discount is given. Check if 
+customer has a membership with the store. If he is member, then he gets an extra 5% discount on every purchase, but if purchase amount of member is above Rs.1000 then he gets 
+total discount of 15% on purchase price. For example, if RaJesh does purchase of Rs.1800 which is above Rs.1000 and he is not member of store he gets discount of 10%.
+But If Rajesh Is member of store then he gets total 15% discount on Rs.1800 since its above Rs.1000, but if he does purchase of Rs.800 which Is below Rs.1000 then he gets only 
+discount of 5%.
 Given , Items and price
 Item No Items price
 1. Yoghurt 50 
