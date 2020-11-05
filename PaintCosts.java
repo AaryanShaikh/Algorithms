e339f30cd41e6b5b488f3812023cb3ef5318c0e3
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-11-05 17:42:58 +0530

    Update PaintCosts.java

--- PaintCosts.java
+++ PaintCosts.java
@@ -11,14 +11,10 @@ import java.util.*;
 public class PaintCosts{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		float num = in.nextFloat();
-		float res = num*5;
-		res = res + 40;
-		float res1 = res / 100;
-		res1 = res1*10;
-		res = res1 + res;
-		res = Math.ceil(res);
-		int resf = (int)res; 
-		System.out.println(resf);
+		int cost = in.nextInt();
+		double color = cost*5.0;
+		double res = 40+color;
+		double tax = (10.0*res)/100.0;
+		System.out.println(Math.round(res+tax));
 	}
 }
