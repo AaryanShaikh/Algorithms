28421acf71939ca67dc47e8ee5577f8f35596e42
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2021-01-15 17:19:09 +0530

    Create Rev.java

---
+++ Sololearn/Rev.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Rev{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println(new StringBuffer(in.nextLine()).reverse().toString());
+	}
+}
