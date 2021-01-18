8ef610ce8caef766f9591ef3568241019cbe58cd
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2021-01-18 13:43:44 +0530

    Delete TimeConverter.java

--- Sololearn/TimeConverter.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class TimeConverter{
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println(((in.nextInt()*24)*60)*60);
-	}
-}
