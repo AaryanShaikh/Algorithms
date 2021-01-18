99e709ee9e79c1bbb313c29c98ceeb122bbbcf62
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2021-01-18 13:43:16 +0530

    Delete Rev.java

--- Sololearn/Rev.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class Rev{
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println(new StringBuffer(in.nextLine()).reverse().toString());
-	}
-}
