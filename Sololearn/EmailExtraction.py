96589fca845e59868be8df3a194f423ff3bf504e
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2021-01-18 13:39:26 +0530

    Delete EmailExtraction.py

--- Sololearn/EmailExtraction.py
+++
@@ -1,7 +0,0 @@
-import re
-
-pattern = r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)"
-str = "Please contact aayanshaikh27638@gmail.com for assistance"
-match = re.search(pattern, str)
-if match:
-    print(match.group())
